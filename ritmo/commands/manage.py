69c473caa4bda9258b12b960c41a392b4571925c
daniarlert <arlertdaniel@gmail.com>
2023-01-04 19:03:35 +0100

    feat: add commands to mark as done/undone habits

--- ritmo/commands/manage.py
+++ ritmo/commands/manage.py
@@ -24,7 +24,7 @@ def mark_done(name: str):
                 sess.query(models.HabitDay)
                 .filter(
                     models.HabitDay.habit_id == habit.id
-                    and models.HabitDay.date == datetime.datetime.utcnow()
+                    and models.HabitDay.date == datetime.datetime.utcnow().date
                 )
                 .first()
             )
@@ -32,22 +32,48 @@ def mark_done(name: str):
             if habit_day:
                 habit_day.completed = True
 
+                if habit.type == "numerical":
+                    habit_day.completed_num += 1
             else:
-                habit_day = sess.add(models.HabitDay(habit_id=habit.id, completed=True))
-
-            if habit.type == "numerical":
-                habit_day.completed_num = habit_day.completed_num + 1
+                habit_day = models.HabitDay(
+                    habit_id=habit.id, completed=True, completed_num=1
+                )
+                sess.add(habit_day)
 
             sess.commit()
         else:
             click.echo(f"Habit '{name}' not found.")
 
 
-@click.command(name="undone", help="Mark a habit as undone.")
+@click.command(name="undo", help="Mark a habit as undone.")
 @click.argument("name", nargs=1, type=str)
 def mark_undone(name: str):
     """
     Mark a habit as undone.
     """
 
-    pass
+    Session = session.create_session("sqlite:///habit.db")
+    with Session.begin() as sess:
+        habit: models.Habit = (
+            sess.query(models.Habit).filter(models.Habit.name == name).first()
+        )
+
+        if habit:
+            habit_day: models.HabitDay = (
+                sess.query(models.HabitDay)
+                .filter(
+                    models.HabitDay.habit_id == habit.id
+                    and models.HabitDay.date == datetime.datetime.utcnow().date
+                )
+                .first()
+            )
+
+            if habit_day:
+                habit_day.completed = False
+
+                if habit.type == "numerical" and habit_day.completed_num > 0:
+                    habit_day.completed_num -= 1
+
+            sess.commit()
+        else:
+            click.echo(f"Habit '{name}' not found.")
