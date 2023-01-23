5c63204012d0b5769ddf33a6a1bebef35d3f9fe8
daniarlert <arlertdaniel@gmail.com>
2023-01-23 22:55:41 +0100

    feat: add sort option to list command

--- ritmo/commands/list_habit.py
+++ ritmo/commands/list_habit.py
@@ -8,12 +8,14 @@ from ritmo.models import Habit
 from ritmo.sessions import create_local_session
 
 
-def list_habit(sess: Session) -> None:
+def list_habit(sess: Session, sort_by: str, reverse: bool) -> None:
     """
     List all habits.
 
     Args:
         sess: The database session.
+        sort_by: Sorting criteria
+        reverse: Reverse sorting criteria
     """
 
     table = Table(show_header=True, title="Habits")
@@ -24,6 +26,20 @@ def list_habit(sess: Session) -> None:
     table.add_column("End date")
 
     habits = sess.query(Habit).all()
+    if len(habits) == 0:
+        click.echo("No habits found")
+        return
+
+    if sort_by == "name":
+        habits.sort(key=lambda habit: habit.name, reverse=reverse)
+    elif sort_by == "start-date":
+        print("sorting by date")
+        habits.sort(key=lambda habit: habit.start_date, reverse=reverse)
+    elif sort_by == "end-date":
+        habits.sort(
+            key=lambda habit: (habit.end_date is None, habit.end_date), reverse=reverse
+        )
+
     for habit in habits:
         table.add_row(
             habit.name,
@@ -38,8 +54,22 @@ def list_habit(sess: Session) -> None:
 
 
 @click.command(name="list", help="List habits.")
+@click.option(
+    "--sort-by",
+    type=click.Choice(["name", "start-date", "end-date"], case_sensitive=True),
+    help="Sorting criteria",
+    default="name",
+    show_default=True,
+)
+@click.option(
+    "--reverse",
+    is_flag=True,
+    help="Reverse sorting criteria",
+    default=False,
+    show_default=True,
+)
 @with_sqlalchemy_error_handling
-def list_habit_cmd(description: bool, start_date: bool, end_date: bool):
+def list_habit_cmd(sort_by: str, reverse: bool):
     local_session = create_local_session()
     with local_session.begin() as sess:
-        list_habit(sess)
+        list_habit(sess, sort_by, reverse)
